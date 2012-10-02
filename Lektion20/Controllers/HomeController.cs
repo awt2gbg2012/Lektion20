1f275d5eca1867aac18ea64051cdd27e1d599355
David Gullmarsvik <awt2.2012@gmail.com>
2012-10-02 11:03:21 +0200

    Uppgift 7

--- Lektion20/Controllers/HomeController.cs
+++ Lektion20/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using Lektion20.Models.Repositories.Abstract;
 using Lektion20.Models.Entities;
+using System.Configuration;
+using System.Net;
+using DotNetOpenAuth.ApplicationBlock.Facebook;
 
 namespace Lektion20.Controllers
 {
@@ -25,7 +28,29 @@ namespace Lektion20.Controllers
 
         public ActionResult About()
         {
-            return View();
+            string result = "";
+            var facebookAppOwnerID = ConfigurationManager.AppSettings["facebookAppOwnerID"];
+            var appOwner = _userRepo.FindAll(u => u.FacebookID.ToString() == facebookAppOwnerID)
+                .FirstOrDefault();
+            if (null != appOwner)
+            {
+                var longTermAccessToken = appOwner.LongTermAccessToken;
+                var request = WebRequest.Create(
+                    string.Format(@"https://graph.facebook.com/{0}/feed?access_token={1}",
+                    facebookAppOwnerID,
+                    Uri.EscapeDataString(longTermAccessToken)));
+                using (var response = request.GetResponse())
+                {
+                    using (var responseStream = response.GetResponseStream())
+                    {
+                        var graph = FacebookFeedGraph.Deserialize(responseStream);
+                        result = HttpUtility.HtmlEncode(graph.Posts
+                            .Where(p => p.From.ID == facebookAppOwnerID)
+                            .FirstOrDefault().Message);
+                    }
+                }
+            }
+            return View((object)result);
         }
     }
 }
